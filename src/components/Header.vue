3166503059bdc67e1cb4adf136110fd2b34dcb96
RayanaAbdrimova <148768361+RayanaAbdrimova@users.noreply.github.com>
2023-10-23 16:43:27 +0500

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">github finder</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
